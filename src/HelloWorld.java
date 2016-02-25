f779f92cc1f2c2320f1ee5d39080e3b4b580427a
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-25 03:12:37 -0800

    use substring function in java

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by TrungNT on 2/25/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        String a = "1.Trung";
+        String b = "2.Tran";
+        System.out.println(a.substring(2) + " " + b.substring(2));
+    }
+}
